eee35fe4e7b427aaf91137b3e40a046dde47819a
Natasha Teng <nteng3290@gmail.com>
2018-02-09 16:36:24 -0500

    Most JS is working!!!!!

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 //create an object with my "options for BBT"
 
+//start with all the options and push it in a global array
+//then write a function that allows me to call everytime a new option is clicked
+//based on those option in the global array at the corresponding class 
 
-//if they user value picks green then it turns green, else it turns black
-// pickABase = function (){}
 
-const teaPlease = function (x,y){
-    const userBase = $("input[name=base]:checked").val();
-    console.log(userBase);
-}
 //Tea Options
 $(".teas").on("click", function(){
+    const userBoba = [];
     const userBase = $("input[name=base]:checked").val();
     console.log(userBase);
+    userBoba.push(userBase);
+    // console.log(userBoba);
     if (userBase === "green"){
         $(".contents").addClass("greentea");
         $(".contents").removeClass("blacktea");
@@ -27,56 +27,57 @@ $(".teas").on("click", function(){
 //Milk Options
 $(".milks").on("click", function () {
     const userMilk = $("input[name=milk]:checked").val();
-    console.log(userMilk);
-    if (userMilk === "yesMilk" && $(".contents").hasClass("greentea")) {
-        $(".contents").addClass("milkgreen");
-        $(".contents").removeClass("milkblack");
-    }
-    else if (userMilk === "yesMilk" && $(".contents").hasClass("blacktea")) {
-        $(".contents").addClass("milkblack");
-        $(".contents").removeClass("milkgreen");
+    if (userMilk === "yesMilk") {
+        $(".contents").addClass("milk");
     }
     else {
-        $(".contents").removeClass("milkblack milkgreen");
+        $(".contents").removeClass("milk");
     }
 });
 //Flavour Options
 $(".flavour").on("click", function () {
     const userFlavour = $("input[name=flavour]:checked").val();
-    console.log(userFlavour);
     if (userFlavour === "taro") {
         // This is a test
         $(".contents").addClass("taro");
-        $(".contents").removeClass("honeydew");
-        $(".contents").removeClass("strawberry");
+        $(".contents").removeClass("honeydew strawberry rose grape peach");
     }
     else if (userFlavour === "honeyDew") {
         $(".contents").addClass("honeydew");
-        $(".contents").removeClass("taro");
-        $(".contents").removeClass("strawberry");
-        // $(".contents").removeClass("greentea");
+        $(".contents").removeClass("taro strawberry rose grape peach");
     }
     else if (userFlavour === "strawberry") {
         $(".contents").addClass("strawberry");
-        $(".contents").removeClass("taro");
-        $(".contents").removeClass("honeydew");
-        // $(".contents").removeClass("greentea");
+        $(".contents").removeClass("taro honeydew rose grape peach");
     }
     //There is a glitch that if you change your previous option the "new" option won't change unless clicked again
-    else if (userFlavour === "rose" && $(".contents").hasClass("blacktea")) {
-        $(".contents").addClass("roseblack");
-        $(".contents").removeClass("rosegreen");
+    else if (userFlavour === "rose") {
+        $(".contents").addClass("rose");
+        $(".contents").removeClass("grape peach taro honeydew strawberry");
     }
-    else if (userFlavour === "rose" && $(".contents").hasClass("greentea")) {
-        $(".contents").addClass("rosegreen");
-        $(".contents").removeClass("roseblack");
+    else if (userFlavour === "grape") {
+        $(".contents").addClass("grape");
+        $(".contents").removeClass("rose peach taro honeydew strawberry");
+    }
+    else if (userFlavour === "peach") {
+        $(".contents").addClass("peach");
+        $(".contents").removeClass("rose grape taro honeydew strawberry");
     }
     else {
-        $(".contents").addClass("transparent");
+        $(".contents").removeClass("taro honeydew strawberry rose grape peach");
     }
 });
 
-
+//Topping Options
+$(".toppings").on("click", function () {
+    const userTopping = $("input[name=toppings]:checked").val();
+    if (userTopping === "pearl") {
+        $(".addins").addClass("pearl");
+    }
+    else {
+        $(".addins").removeClass("");
+    }
+});
 
 //want to append a Colour on the page with the Value is selected
 
