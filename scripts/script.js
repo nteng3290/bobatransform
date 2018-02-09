e06d6ad5d772d369bf622c99b7ebcd45d9dd3d8e
Natasha Teng <nteng3290@gmail.com>
2018-02-08 23:20:18 -0500

    JavaScript getting there, setting my conditions for Flavour Value

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 //create an object with my "options for BBT"
 
-console.log("Hello");
 
 //if they user value picks green then it turns green, else it turns black
 // pickABase = function (){}
@@ -9,53 +8,84 @@ const teaPlease = function (x,y){
     const userBase = $("input[name=base]:checked").val();
     console.log(userBase);
 }
-
+//Tea Options
 $(".teas").on("click", function(){
     const userBase = $("input[name=base]:checked").val();
     console.log(userBase);
     if (userBase === "green"){
-        $(".third").addClass("greentea");
-        $(".third").removeClass("blacktea");
+        $(".contents").addClass("greentea");
+        $(".contents").removeClass("blacktea");
     }
     else if (userBase === "black"){
-        $(".third").addClass("blacktea");
-        $(".third").removeClass("greentea");
+        $(".contents").addClass("blacktea");
+        $(".contents").removeClass("greentea");
     }
     else{
-        $(".third").removeClass("blacktea greentea");
+        $(".contents").removeClass("blacktea greentea");
     }
 });
+//Milk Options
 $(".milks").on("click", function () {
     const userMilk = $("input[name=milk]:checked").val();
     console.log(userMilk);
-    if (userMilk === "yesMilk") {
-        $(".third").addClass("milk");
+    if (userMilk === "yesMilk" && $(".contents").hasClass("greentea")) {
+        $(".contents").addClass("milkgreen");
+        $(".contents").removeClass("milkblack");
     }
-    else if (userMilk === "noMilk") {
-        $(".third").removeClass("milk");
+    else if (userMilk === "yesMilk" && $(".contents").hasClass("blacktea")) {
+        $(".contents").addClass("milkblack");
+        $(".contents").removeClass("milkgreen");
     }
     else {
-        $(".third").removeClass("milk");
+        $(".contents").removeClass("milkblack milkgreen");
     }
 });
-// $(".addFlavourNoMilk").on("click", function () {
-//     const userFlavour = $("input[name=milk]:checked").val();
-//     console.log(userMilk);
-//     if (userMilk === "yesMilk") {
-//         $(".third").addClass("milk");
-//     }
-//     else if (userMilk === "noMilk") {
-//         $(".third").removeClass("milk");
-//     }
-//     else {
-//         $(".third").removeClass("milk");
-//     }
-// });
+//Flavour Options
+$(".flavour").on("click", function () {
+    const userFlavour = $("input[name=flavour]:checked").val();
+    console.log(userFlavour);
+    if (userFlavour === "taro") {
+        // This is a test
+        $(".contents").addClass("taro");
+        $(".contents").removeClass("honeydew");
+        $(".contents").removeClass("strawberry");
+    }
+    else if (userFlavour === "honeyDew") {
+        $(".contents").addClass("honeydew");
+        $(".contents").removeClass("taro");
+        $(".contents").removeClass("strawberry");
+        // $(".contents").removeClass("greentea");
+    }
+    else if (userFlavour === "strawberry") {
+        $(".contents").addClass("strawberry");
+        $(".contents").removeClass("taro");
+        $(".contents").removeClass("honeydew");
+        // $(".contents").removeClass("greentea");
+    }
+    //There is a glitch that if you change your previous option the "new" option won't change unless clicked again
+    else if (userFlavour === "rose" && $(".contents").hasClass("blacktea")) {
+        $(".contents").addClass("roseblack");
+        $(".contents").removeClass("rosegreen");
+    }
+    else if (userFlavour === "rose" && $(".contents").hasClass("greentea")) {
+        $(".contents").addClass("rosegreen");
+        $(".contents").removeClass("roseblack");
+    }
+    else {
+        $(".contents").addClass("transparent");
+    }
+});
 
 
 
 //want to append a Colour on the page with the Value is selected
 
+const buttonShow = function (optionSelected, showOptions) {
+    $(optionSelected).on("click", function (e) {
+        e.preventDefault();
+        $(showOptions).removeClass("hidden");
+    });
+}
 const hide = function(optionSelected, showOptions){
     $(optionSelected).on("click", function(){
         $(showOptions).removeClass("hidden");
@@ -64,12 +94,11 @@ const hide = function(optionSelected, showOptions){
 
 
 $(function(){
-    hide("#teaBase", ".teas");
-    $("#teaBase").on("click", function (e) {
-        e.preventDefault();
-    });
+    buttonShow("#teaBase", ".teas");
+    buttonShow("#addMilk", ".milks");
     hide("#black", ".milks");
     hide("#green",".milks");
+
     hide("#yesMilk",".addFlavourWithMilk");
     $("#yesMilk").on("click", function(){
         $(".addFlavourNoMilk").addClass("hidden");
@@ -78,6 +107,7 @@ $(function(){
     $("#noMilk").on("click",function(){
         $(".addFlavourWithMilk").addClass("hidden");
     });
+    
     hide(".flavourShot",".toppings");
     
     // On click display options
