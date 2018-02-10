2bbe45f8cc833edd41d5a011e4585fc96e3e0c5b
Natasha Teng <nteng3290@gmail.com>
2018-02-10 15:53:22 -0500

    Adding more SASS design

--- scripts/script.js
+++ scripts/script.js
@@ -4,45 +4,49 @@
 //then write a function that allows me to call everytime a new option is clicked
 //based on those option in the global array at the corresponding class 
 
-
 //Tea Options
 $(".teas").on("click", function(){
-    const userBoba = [];
     const userBase = $("input[name=base]:checked").val();
     console.log(userBase);
-    userBoba.push(userBase);
-    // console.log(userBoba);
     if (userBase === "green"){
+        $(".greenTea").addClass("selected");
+        $(".blackTea").removeClass("selected");
         $(".contents").addClass("greentea");
         $(".contents").removeClass("blacktea");
     }
     else if (userBase === "black"){
+        $(".blackTea").addClass("selected");
+        $(".greenTea").removeClass("selected");
         $(".contents").addClass("blacktea");
         $(".contents").removeClass("greentea");
-    }
-    else{
-        $(".contents").removeClass("blacktea greentea");
     }
 });
 //Milk Options
 $(".milks").on("click", function () {
     const userMilk = $("input[name=milk]:checked").val();
     if (userMilk === "yesMilk") {
+        $(".yesMilk").addClass("selected");
+        $(".noMilk").removeClass("selected");
         $(".contents").addClass("milk");
     }
     else {
+        $(".noMilk").addClass("selected");
+        $(".yesMilk").removeClass("selected");
         $(".contents").removeClass("milk");
     }
 });
+
 //Flavour Options
 $(".flavour").on("click", function () {
     const userFlavour = $("input[name=flavour]:checked").val();
     if (userFlavour === "taro") {
-        // This is a test
+        // $(".flavour.taro").addClass("selected");
+        // $(".flavour").removeClass("selected");
         $(".contents").addClass("taro");
         $(".contents").removeClass("honeydew strawberry rose grape peach");
     }
     else if (userFlavour === "honeyDew") {
+        // $(".flavour.honeyDew").addClass("selected");
         $(".contents").addClass("honeydew");
         $(".contents").removeClass("taro strawberry rose grape peach");
     }
@@ -50,7 +54,6 @@ $(".flavour").on("click", function () {
         $(".contents").addClass("strawberry");
         $(".contents").removeClass("taro honeydew rose grape peach");
     }
-    //There is a glitch that if you change your previous option the "new" option won't change unless clicked again
     else if (userFlavour === "rose") {
         $(".contents").addClass("rose");
         $(".contents").removeClass("grape peach taro honeydew strawberry");
@@ -67,7 +70,6 @@ $(".flavour").on("click", function () {
         $(".contents").removeClass("taro honeydew strawberry rose grape peach");
     }
 });
-
 //Topping Options
 $(".toppings").on("click", function () {
     const userTopping = $("input[name=toppings]:checked").val();
@@ -79,7 +81,6 @@ $(".toppings").on("click", function () {
     }
 });
 
-//want to append a Colour on the page with the Value is selected
 
 const buttonShow = function (optionSelected, showOptions) {
     $(optionSelected).on("click", function (e) {
@@ -96,7 +97,9 @@ const hide = function(optionSelected, showOptions){
 
 $(function(){
     buttonShow("#teaBase", ".teas");
-    buttonShow("#addMilk", ".milks");
+    $("#teaBase").on("click", function(){
+        $(".start").addClass("hidden");
+    });
     hide("#black", ".milks");
     hide("#green",".milks");
 
@@ -111,6 +114,20 @@ $(function(){
     
     hide(".flavourShot",".toppings");
     
+    $(".finalize").on("click", function(e){
+        e.preventDefault();
+        $("fieldset").toggleClass("hidden");
+        $(".nameDrink").removeClass("hidden");
+        $(".finalize").addClass("hidden");
+        $(".done").removeClass("hidden");
+    });
+    $(".done").on("click",function(e){
+        e.preventDefault();
+        let userName = $("input[name=nameIt]").val();
+        $(".addons").addClass("hidden");
+        $("h2").append(`${userName}`);
+        $(".replay").removeClass("hidden");
+    });
     // On click display options
     // when option is selected next button is opened with options 
 });
